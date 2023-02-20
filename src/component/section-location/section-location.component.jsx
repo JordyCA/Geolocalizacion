c8480b13dffa91713d7ba70abf65a8bd8fd1463f
JordyCA <jordycastroavendano@gmail.com>
2023-02-19 22:35:26 -0600

    Add save in array positions

--- src/component/section-location/section-location.component.jsx
+++ src/component/section-location/section-location.component.jsx
@@ -3,20 +3,26 @@ import { useState } from "react";
 
 const dataLocationGeneral =
 {
+    positions: [],
     latitud: null,
     longitud: null
 }
 
 const SectionLocation = () => {
     const [dataLocation, setDataLocation] = useState(dataLocationGeneral);
-
+   // console.log(dataLocation.positions.length);
     const onClickAction = () => {
+        console.log(dataLocation.positions);
         if ("geolocation" in navigator) {
             const getNavigacion = () => {
                 navigator.geolocation.getCurrentPosition((position) => {
                     let dataTemp = Object.assign({}, dataLocation);
-                    dataTemp.latitud = position.coords.latitude;
-                    dataTemp.longitud = position.coords.longitude;
+
+                    let latitud = position.coords.latitude;
+                    let longitud = position.coords.longitude;
+
+                    dataTemp.positions.push({'latitud' : latitud, 'longitud' : longitud});
+                   
                     setDataLocation(dataTemp);
                 });
             }
@@ -29,9 +35,10 @@ const SectionLocation = () => {
     return (
         <div>
             {
-                (dataLocation.latitud !== null && dataLocation.longitud !== null) ? 
-                    <p>{`Latitud: ${dataLocation.latitud} --- Longitud ${dataLocation.longitud}`}</p> : 
-                    <p>No se tiene información de geolocalización.</p>
+                (dataLocation.positions.length) &&
+                     dataLocation.positions.map(element => {
+                        return (<p>{`Latitud: ${element.latitud} --- Longitud ${element.longitud}`}</p>)
+                    })
             }
             <button onClick={onClickAction}>LOCATION NOW</button>
         </div>
